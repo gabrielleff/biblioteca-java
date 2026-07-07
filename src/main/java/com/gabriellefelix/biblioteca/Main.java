65e5e6ffa2137f2cc9fb093620b9a01b1a8b6e3d
Gabrielle <gabriellefelixferr@gmail.com>
2026-07-07 19:45:14 -0300

    test: add initial application flow

--- src/main/java/com/gabriellefelix/biblioteca/Main.java
+++ src/main/java/com/gabriellefelix/biblioteca/Main.java
@@ -1,14 +1,54 @@
 package com.gabriellefelix.biblioteca;
-import com.gabriellefelix.biblioteca.model.Livro;
+
+import com.gabriellefelix.biblioteca.model.*;
+import com.gabriellefelix.biblioteca.repository.*;
+import com.gabriellefelix.biblioteca.service.BibliotecaService;
 
 public class Main {
+
     public static void main(String[] args) {
-        Livro livro = new Livro("Clean Code",
+
+        LivroRepository livroRepository = new LivroRepository();
+        UsuarioRepository usuarioRepository = new UsuarioRepository();
+        EmprestimoRepository emprestimoRepository = new EmprestimoRepository();
+
+        BibliotecaService bibliotecaService = new BibliotecaService(
+                livroRepository,
+                usuarioRepository,
+                emprestimoRepository
+        );
+
+
+        Livro livro1 = new Livro(
+                "Clean Code",
                 "Robert C. Martin",
                 "9780132350884",
                 2008,
-                464);
+                464
+        );
 
-        System.out.println(livro);
+        Usuario usuario1 = new Usuario(
+                "Gabrielle",
+                "gabrielle@email.com",
+                "83999999999"
+        );
+
+
+        bibliotecaService.cadastrarLivro(livro1);
+
+        bibliotecaService.cadastrarUsuario(usuario1);
+
+
+        bibliotecaService.emprestarLivro(
+                "9780132350884",
+                usuario1.getId()
+        );
+
+
+        System.out.println("Livro emprestado com sucesso!");
+
+        System.out.println(
+                bibliotecaService.buscarHistoricoUsuario(usuario1.getId())
+        );
     }
 }
